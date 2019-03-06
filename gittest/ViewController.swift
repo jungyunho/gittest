252b314fde1e6d83278dea36754ab1cd1a6cba8c
D7703_13 <d7703_13@d7703-13ui-imac.local>
2019-03-06 16:18:44 +0900

    Initial Commit

---
+++ gittest/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  gittest
+//
+//  Created by D7703_13 on 2019. 3. 6..
+//  Copyright © 2019년 personel team. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor.blue
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
